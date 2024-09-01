acdbbec1fb63b2a01db6116ac6348ce97533e2a1
yusasa16 <yusasa16@gmail.com>
2024-09-01 22:28:50 +0900

    feat: permalinkの調整

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,11 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(require("./src/_11ty/plugins/index.cjs"));
 
+	eleventyConfig.addGlobalData("permalink", () => {
+		return (data) =>
+			`${data.page.filePathStem}.${data.page.outputFileExtension}`;
+	});
+
 	return {
 		dir: {
 			input: "./src/input",
